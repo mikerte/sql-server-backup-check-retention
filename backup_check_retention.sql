6fd92ad4a0da4d6e86dd04f9f0d28ae5011c9bc4
mikerte <40292671+mikerte@users.noreply.github.com>
2025-07-02 08:40:48 +0200

    Update backup_check_retention.sql

--- backup_check_retention.sql
+++ backup_check_retention.sql
@@ -1,7 +1,7 @@
 USE msdb;
 GO
 
--- Step 1: Get CleanupTime per Job and map it to BackupType
+-- STEP 1: Get CleanupTime values per Ola Hallengren job type
 ;WITH CleanupTimes AS (
     SELECT 
         sj.name AS JobName,
@@ -23,58 +23,67 @@ GO
                                 - (CHARINDEX('@CleanupTime', sjs.command) + LEN('@CleanupTime ='))
                         )
                     )),
-                    '''', ''  -- remove single quotes
+                    '''', ''
                 ),
-                '"', ''      -- remove double quotes
+                '"', ''
             ) AS INT
         ) AS CleanupHours
     FROM msdb.dbo.sysjobs sj
     INNER JOIN msdb.dbo.sysjobsteps sjs ON sj.job_id = sjs.job_id
-    WHERE sjs.command LIKE '%DatabaseBackup%'
-      AND sjs.command LIKE '%@CleanupTime%'
+    WHERE sjs.command LIKE '%DatabaseBackup%' AND sjs.command LIKE '%@CleanupTime%'
+),
+
+-- STEP 2: Add backup category to each backup record
+BackupHistory AS (
+    SELECT
+        b.database_name,
+        b.backup_start_date,
+        b.backup_finish_date,
+        b.backup_size,
+        b.type,
+        b.recovery_model,
+        mf.physical_device_name,
+        CASE 
+            WHEN b.type = 'D' AND b.database_name IN ('master', 'model', 'msdb') THEN 'SystemFull'
+            WHEN b.type = 'D' THEN 'UserFull'
+            WHEN b.type = 'I' THEN 'UserDiff'
+            WHEN b.type = 'L' THEN 'UserLog'
+            ELSE 'Other'
+        END AS BackupCategory
+    FROM msdb.dbo.backupset b
+    INNER JOIN msdb.dbo.backupmediafamily mf ON b.media_set_id = mf.media_set_id
+),
+
+-- STEP 3: Pick only the latest backup per DB and type
+LatestBackup AS (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY database_name, BackupCategory ORDER BY backup_start_date DESC) AS rn
+    FROM BackupHistory
 )
 
--- Step 2: Backup history + Match backup type to CleanupTimes
+-- STEP 4: Final select with CleanupTime and status
 SELECT
     SERVERPROPERTY('MachineName') AS ServerName,
-    b.database_name,
-    CASE b.type
+    lb.database_name,
+    CASE lb.type
         WHEN 'D' THEN 'Full'
         WHEN 'I' THEN 'Differential'
         WHEN 'L' THEN 'Log'
-        ELSE b.type
+        ELSE lb.type
     END AS BackupType,
-    b.backup_start_date,
-    b.backup_finish_date,
-    DATEDIFF(MINUTE, b.backup_start_date, b.backup_finish_date) AS DurationMinutes,
-    CAST(b.backup_size / 1048576.0 AS DECIMAL(18,2)) AS BackupSizeMB,
-    mf.physical_device_name AS BackupFile,
-    b.recovery_model,
-    
-    -- Determine backup category to join with CleanupTime
-    CASE 
-        WHEN b.type = 'D' AND b.database_name IN ('master', 'model', 'msdb') THEN 'SystemFull'
-        WHEN b.type = 'D' THEN 'UserFull'
-        WHEN b.type = 'I' THEN 'UserDiff'
-        WHEN b.type = 'L' THEN 'UserLog'
-        ELSE 'Other'
-    END AS BackupCategory,
-
-    ct.CleanupHours,
+    lb.backup_start_date,
+    lb.backup_finish_date,
+    DATEDIFF(MINUTE, lb.backup_start_date, lb.backup_finish_date) AS DurationMinutes,
+    CAST(lb.backup_size / 1048576.0 AS DECIMAL(18,2)) AS BackupSizeMB,
+    lb.physical_device_name AS BackupFile,
+    lb.recovery_model,
+    ct.CleanupHours AS RetentionHours,
     CASE 
         WHEN ct.CleanupHours IS NULL THEN 'Unknown (No CleanupTime)'
-        WHEN b.backup_start_date < DATEADD(HOUR, -ct.CleanupHours, GETDATE()) THEN 'Expired'
+        WHEN lb.backup_start_date < DATEADD(HOUR, -ct.CleanupHours, GETDATE()) THEN 'Expired'
         ELSE 'Valid'
     END AS RetentionStatus
-
-FROM msdb.dbo.backupset b
-INNER JOIN msdb.dbo.backupmediafamily mf ON b.media_set_id = mf.media_set_id
-LEFT JOIN CleanupTimes ct 
-    ON (
-        (b.type = 'D' AND b.database_name IN ('master', 'model', 'msdb') AND ct.BackupCategory = 'SystemFull')
-        OR (b.type = 'D' AND b.database_name NOT IN ('master', 'model', 'msdb') AND ct.BackupCategory = 'UserFull')
-        OR (b.type = 'I' AND ct.BackupCategory = 'UserDiff')
-        OR (b.type = 'L' AND ct.BackupCategory = 'UserLog')
-    )
-WHERE b.backup_start_date >= DATEADD(DAY, -30, GETDATE())
-ORDER BY b.database_name, b.backup_start_date DESC;
+FROM LatestBackup lb
+LEFT JOIN CleanupTimes ct ON lb.BackupCategory = ct.BackupCategory
+WHERE lb.rn = 1
+ORDER BY lb.database_name, BackupType;
